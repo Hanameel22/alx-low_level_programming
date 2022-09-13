a8709f8c2da260f2765ec21a3d37593a2a7ee001
Hanameel <hanameelifeoluwaoniyide@gmail.com>
2022-09-12 21:55:16 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - put s instead of print f
  *
- * Return: Always 0 (Success)*/
+ * Return: 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	return (0);
+puts("\"Programming is like building amultilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-name
+put
